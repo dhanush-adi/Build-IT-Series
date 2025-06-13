ea675d663b3ff0127397c21d83b898463afaf6cf
Dhanush <dhanushvallalar@gmail.com>
2025-06-13 13:19:42 +0530

    Initial commit: Adding portfolio website

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Simple typed text effect
+const typedText = document.getElementById('typed-text');
+const phrases = ["A Developer.", "A Learner.", "An Innovator."];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+  typedText.innerHTML = currentPhrase.join('');
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop();
+      j--;
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
+  }
+  const speed = isEnd ? 2000 : isDeleting ? 50 : 100;
+  setTimeout(loop, speed);
+}
+
+loop();
